583a10a09fedb118e15c70e6226c4c1dd8e6529a
Costin <virlan.costin.andrei@gmail.com>
2023-07-04 09:29:53 +0300

    Small modifications

--- src/index.jsx
+++ src/index.jsx
@@ -9,11 +9,11 @@ import "Styles/reset.css";
 function init() {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <React.StrictMode>
+    <>
       <Header />
       <Content />
       <Footer />
-    </React.StrictMode>
+    </>
   );
 }
 
